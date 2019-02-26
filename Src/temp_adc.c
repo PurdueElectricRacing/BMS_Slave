764e8e6831c86b4b2a41304bfe13debc60e6e1df
mflanag <mflanag@purdue.edu>
2019-02-26 01:31:19 -0500

    Temp adc driver header done

--- Src/temp_adc.c
+++ Src/temp_adc.c
@@ -6,9 +6,18 @@
  */
 #include "temp_adc.h"
 
+uint8_t temp_array[READ_MSG_SIZE];
+flag_t conv_complete;
+
 void task_acquire_temp() {
+	Success_t success = SUCCESSFUL;
+	conv_complete = DEASSERTED;
   uint8_t toggle = 0;
-  init_LTC2497(); //don't need to handle an error TODO: (maybe use a while loop)
+  uint8_t i = 0;
+  uint8_t write_data[WRITE_MSG_SIZE];
+  uint16_t temp = 0;
+  success = init_LTC2497(); //don't need to handle an error TODO: (maybe use a while loop)
+
   TickType_t time_init = 0;
   while (1) {
     time_init = xTaskGetTickCount();
@@ -18,17 +27,30 @@ void task_acquire_temp() {
       //todo: read_temp1();
       //todo: process_temp(temp_values* temps);
       //todo: add temps to tx_queue
+    	for (i = 0; i < (NUM_TEMP / 2); i++) {
+    		conv_complete = DEASSERTED;
+    		write_data[0] = ID_TEMP_1 << 1 | WRITE_ENABLE;
+    		//HAL_I2C_Master_Transmit(&hi2c1, , )
+    		//write a channel select blocking
+    		//read in nonblocking mode 24 bits
+    		//store the data
+    	}
+
     } else {
       //process temp2
       //todo: read_temp2();
       //todo: process_temp(temp_values* temps);
       //todo: add temps to tx_queue
     }
+
+    //pack the data into can msgs
+    //send out
+
     vTaskDelayUntil(&time_init, ACQUIRE_TEMP_RATE);
   }
 }
 
-HAL_StatusTypeDef init_LTC2497() {
+Success_t init_LTC2497() {
   HAL_StatusTypeDef success1 = HAL_OK;
   HAL_StatusTypeDef success2 = HAL_OK;
   //try to initialize communication with the I2C chip
@@ -54,5 +76,5 @@ HAL_StatusTypeDef init_LTC2497() {
     bms.temp2_con = 1;
   }
   
-  return success1 && success2;
+  return (Success_t) (success1 && success2);
 }
